4054f784c3dcf382e260f3fbfb1dc4b5ca0605eb
CplJeebus <26419780+CplJeebus@users.noreply.github.com>
2020-12-11 01:54:49 +0000

    WIP starting to figure out containers
    - Simple Docker file
    - Build helper script
    - Simple http listener.
    
    [#176006358](https://www.pivotaltracker.com/story/show/176006358)

--- src/myeircode/build.sh
+++ src/myeircode/build.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
 
+build_local(){
 go build -o ../../myeircode main.go
+}
+
+docker_build(){
+docker build --tag myeircode:00-alpha .
+}
+
+main(){
+  case $1 in
+  "local")
+      build_local
+      ;;
+  "docker")
+    export GOOS=linux
+    export GOARCH=amd64
+    build_local
+    docker_build
+
+  esac
+}
+
+main "$@"
