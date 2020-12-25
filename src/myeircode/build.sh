9c5c6fb4c1bf31202f09d43847a88aceca08712b
CplJeebus <26419780+CplJeebus@users.noreply.github.com>
2020-12-25 19:02:36 +0000

    Pushed simple container to gcr.
    - created a helper script
    
    [#176182074](https://www.pivotaltracker.com/story/show/176182074)

--- src/myeircode/build.sh
+++ src/myeircode/build.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
 
 
-build_local(){
-go build -o ../../myeircode main.go
-}
-
-docker_build(){
-docker build --tag myeircode:00-alpha .
-}
-
-main(){
-  case $1 in
-  "local")
-      build_local
-      ;;
-  "docker")
-    export GOOS=linux
-    export GOARCH=amd64
-    build_local
-    docker_build
-
-  esac
-}
-
-main "$@"
+go build -o ../../check-ecdc main.go
