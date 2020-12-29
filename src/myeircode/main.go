7a1b50902f72e1ed42200fab74736e423f4ef7c1
CplJeebus <26419780+CplJeebus@users.noreply.github.com>
2020-12-29 05:11:20 +0000

    Refactored some stuff around
    [#176006404](https://www.pivotaltracker.com/story/show/176006404)

--- src/myeircode/main.go
+++ src/myeircode/main.go
@@ -1,72 +1,24 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"io/ioutil"
-	"log"
+	. "myeircode/utils"
 	"net/http"
-	"time"
-
-	"cloud.google.com/go/storage"
-	"google.golang.org/api/iterator"
 )
 
+var c Config
+
 func main() {
-	http.HandleFunc("/", HelloServer)
+	c.LoadConfig()
+	http.HandleFunc("/", ShowCodes)
 	http.ListenAndServe(":8080", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello Server")
-	b, err := downloadFile()
+func ShowCodes(w http.ResponseWriter, r *http.Request) {
+	b, err := DownloadFile(c.Bucket)
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	fmt.Fprintf(w, string(b))
-}
-
-func downloadFile() ([]byte, error) {
-	fmt.Println("Download Time!")
-	bucket := "mybucket"
-	object := "codes.yml"
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("storage.NewClient: %v", err)
-	}
-	defer cancel()
-
-	ctx, cancel = context.WithTimeout(ctx, time.Second*10)
-	defer cancel()
-
-	query := &storage.Query{Prefix: ""}
-
-	var names []string
-	it := client.Bucket(bucket).Objects(ctx, query)
-	for {
-		attrs, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		names = append(names, attrs.Name)
-	}
-
-	fmt.Println(names)
-
-	rc, err := client.Bucket(bucket).Object(object).NewReader(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("Object(%q).NewReader: %v", object, err)
-	}
-	defer rc.Close()
-
-	data, err := ioutil.ReadAll(rc)
-	if err != nil {
-		return nil, fmt.Errorf("ioutil.ReadAll: %v", err)
-	}
-	fmt.Printf("Blob %v downloaded.\n", object)
-	return data, nil
 }
