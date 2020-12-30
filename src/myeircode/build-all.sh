8d628fe3cc17cbefefb214049778b0c7388f1940
CplJeebus <26419780+CplJeebus@users.noreply.github.com>
2020-12-30 23:29:57 +0000

    Some helper scripts.
    [#176318681](https://www.pivotaltracker.com/story/show/176318681)

---
+++ src/myeircode/build-all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+set -e 
+
+buildServer(){
+  export GOOS=linux
+  export GOARCH=amd64
+
+  go build -o myeircode
+}
+
+getLatestTag(){
+  i=$(docker image ls | grep gcr.io | grep myeircode | awk '{print $2}' | cut -d '.' -f 2 | head -n 1)
+  echo $((i+1))
+}
+
+dockerStuff(){
+  latest=$(getLatestTag)
+  base="${GCR_BASE}"
+  
+  docker build --tag ${base}:${latest} .
+  docker push ${base}:${latest}
+}
+
+main(){
+  buildServer
+  dockerStuff
+}
+
+main "@"
